15182b3a0df548174e24400a584c4427b08dcacc
SheldonDeschamps <sheldon.deschamps@polymtl.ca>
2020-09-14 22:49:58 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -27,7 +27,7 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
 
 
 def to_degrees(angle_rads: float) -> tuple:
-    return 
+    return de
 
 
 def to_celsius(temperature: float) -> float:
