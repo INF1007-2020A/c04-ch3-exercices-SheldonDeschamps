adaf3c9613bb28e19bcbf06f0c90c148c766f64e
SheldonDeschamps <sheldon.deschamps@polymtl.ca>
2020-09-14 23:19:53 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -22,7 +22,7 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    rad = radians(angle_degs), 
+    rad = radians(angle_degs), 1rad × (60 × 180)/π
     return rad
 
 
